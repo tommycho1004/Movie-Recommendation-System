64cadaeaacd96309c7617c9eda26dd6c9f15b05e
Jesuisunebaguette <jjx4@scarletmail.rutgers.edu>
2022-02-24 01:04:58 -0500

    added testing functions in main & #3.1

--- hw1.py
+++ hw1.py
@@ -1,5 +1,6 @@
 #Tommy Cho, Jennifer Xin 
 
+import sys
 import math
 from collections import defaultdict
 from collections import Counter
@@ -67,6 +68,11 @@ def get_popular_movies(d, n=10):
     # parameter n: integer (for top n), default value 10
     # return: dictionary that maps movie to average rating
     # WRITE YOUR CODE BELOW
+    sorted_dlist = sorted(d.items(),key = lambda x:x[1], reverse=True) #use d as list, sort, then convert back to dict
+    sorted_ddict = sorted_dlist[:n]
+    output = dict(sorted_ddict)
+
+    return output
 
     
 # 3.2
@@ -75,7 +81,7 @@ def filter_movies(d, thres_rating=3):
     # parameter thres_rating: threshold rating, default value 3
     # return: dictionary that maps movie to average rating
     # WRITE YOUR CODE BELOW
-
+    return
     
 # 3.3
 def get_popular_in_genre(genre, genre_to_movies, movie_to_average_rating, n=5):
@@ -85,7 +91,7 @@ def get_popular_in_genre(genre, genre_to_movies, movie_to_average_rating, n=5):
     # parameter n: integer (for top n), default value 5
     # return: dictionary that maps movie to average rating
     # WRITE YOUR CODE BELOW
-
+    return
     
 # 3.4
 def get_genre_rating(genre, genre_to_movies, movie_to_average_rating):
@@ -94,7 +100,7 @@ def get_genre_rating(genre, genre_to_movies, movie_to_average_rating):
     # parameter movie_to_average_rating: dictionary  that maps movie to average rating
     # return: average rating of movies in genre
     # WRITE YOUR CODE BELOW
-
+    return
     
 # 3.5
 def genre_popularity(genre_to_movies, movie_to_average_rating, n=5):
@@ -103,7 +109,7 @@ def genre_popularity(genre_to_movies, movie_to_average_rating, n=5):
     # parameter n: integer (for top n), default value 5
     # return: dictionary that maps genre to average rating
     # WRITE YOUR CODE BELOW
-
+    return
 
 # ------ TASK 4: USER FOCUSED  --------
 
@@ -112,7 +118,7 @@ def read_user_ratings(f):
     # parameter f: movie ratings file name (e.g. "movieRatingSample.txt")
     # return: dictionary that maps user to movies and ratings
     # WRITE YOUR CODE BELOW
-
+    return
     
 # 4.2
 def get_user_genre(user_id, user_to_movies, movie_to_genre):
@@ -121,7 +127,7 @@ def get_user_genre(user_id, user_to_movies, movie_to_genre):
     # parameter movie_to_genre: dictionary that maps movie to genre
     # return: top genre that user likes
     # WRITE YOUR CODE BELOW
-
+    return
     
 # 4.3    
 def recommend_movies(user_id, user_to_movies, movie_to_genre, movie_to_average_rating):
@@ -131,13 +137,30 @@ def recommend_movies(user_id, user_to_movies, movie_to_genre, movie_to_average_r
     # parameter movie_to_average_rating: dictionary that maps movie to average rating
     # return: dictionary that maps movie to average rating
     # WRITE YOUR CODE BELOW
-
+    return
 
 # -------- main function for your testing -----
-def main():
+def main(argv):
     # write all your test code here
     # this function will be ignored by us when grading
-
+    m_ratings = str(argv[1])
+    m_movies = str(argv[2])
+    print("#1.1 read_ratings_data() => movie to ratings dict \n")
+    movie_rat = read_ratings_data(m_ratings)
+    print(movie_rat)
+    print("\n #1.2 read_movie_genre() => movie to genre dict \n")
+    movie_gen = read_movie_genre(m_movies)
+    print(movie_gen)
+    print("\n #2.1 create_genre_dict() => genre to movies dict \n")
+    gen_movie = create_genre_dict(movie_gen)
+    print(gen_movie)
+    print("\n #2.2 calculate_average_rating() => movie to average rating \n")
+    movie_avg = calculate_average_rating(movie_rat)
+    print(movie_avg)
+    print("\n #3.1 get_popular_movies() => top n movies \n")
+    top_movies = get_popular_movies(movie_avg, 2)
+    print(top_movies)
+    return
     
 # DO NOT write ANY CODE (including variable names) outside of any of the above functions
 # In other words, ALL code your write (including variable names) MUST be inside one of
@@ -146,4 +169,4 @@ def main():
     
 # program will start at the following main() function call
 # when you execute hw1.py
-main()
+main(sys.argv)
